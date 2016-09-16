3cdeb6ed8875904df9e7b43daa2ba9ff3f5e5bbb
sahinibrahimli <sahinibrahimle@gmail.com>
2016-09-16 14:53:42 +0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html><head><title><?php echo "cool title";?></title></head>
 <body
 <?php 
-$image_url='https://www.google.com/intl/en_com/images/srpr/logo3w.png';
+$image_url='https://cloud.githubusercontent.com/assets/22137362/18583178/fc708256-7c19-11e6-90b6-e3816275266a.jpg';
 ?>
 
 <img src="<?php echo $image_url;?>">
