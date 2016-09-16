cba607f261aa5bd63759c0e15179aedbb4fd393c
sahinibrahimli <sahinibrahimle@gmail.com>
2016-09-16 15:26:10 +0400

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,4 @@ header('Accept-Ranges: bytes');
 
 ?>
 
-<a href="https://github.com/sahinibrahimli/menimcv/files/476674/SH.CV.pdf" download="newfilename">Download the pdf</a>
 
-
-<br><h1>Got 123?</h1>
