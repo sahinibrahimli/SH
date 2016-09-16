70eff06c0125aa4524f5250247d7acf2029ac0a0
sahinibrahimli <sahinibrahimle@gmail.com>
2016-09-16 15:07:19 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 
-<?php 
-$image_url='https://github.com/sahinibrahimli/menimcv/files/476674/SH.CV.pdf';
-?>
 
 
+<a href="https://github.com/sahinibrahimli/menimcv/files/476674/SH.CV.pdf" download="newfilename">Download the pdf</a>
 
-<img src="<?php echo $image_url;?>">
-<br><h1>Got asdfaed?</h1>
+
+<br><h1>Got 123?</h1>
