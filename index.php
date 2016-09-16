f300d6e85e6e26ac601c8dc66887cf89959a35cd
sahinibrahimli <sahinibrahimle@gmail.com>
2016-09-16 14:46:37 +0400

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 
 echo "salam cavanlar";
 
+<img src="<?php echo file_dir . '/' . https://cloud.githubusercontent.com/assets/22137362/18583178/fc708256-7c19-11e6-90b6-e3816275266a.jpg; ?>" height="100" width="100"/>
+
+
+
 ?>
