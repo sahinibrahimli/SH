c54c9d763748227757b3a9ed3054731af29defa6
sahinibrahimli <sahinibrahimle@gmail.com>
2016-09-16 15:24:47 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 
 <?php
 
-echo "<iframe src=\"https://github.com/sahinibrahimli/menimcv/files/476674/SH.CV.pdf\" width=\"100%\" style=\"height:100%\"></iframe>";
+$file = 'https://github.com/sahinibrahimli/menimcv/files/476674/SH.CV.pdf';
+$filename = 'SH.CV.pdf';
+header('Content-type: application/pdf');
+header('Content-Disposition: inline; filename="' . $filename . '"');
+header('Content-Transfer-Encoding: binary');
+header('Accept-Ranges: bytes');
+@readfile($file);
+
 
 ?>
 
