1791615794620ae8347156f68fc3bd97cffb7185
sahinibrahimli <sahinibrahimle@gmail.com>
2016-09-16 15:09:17 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 
+<?php
 
+echo "<iframe src=\"https://github.com/sahinibrahimli/menimcv/files/476674/SH.CV.pdf\" width=\"100%\" style=\"height:100%\"></iframe>";
+
+?>
 
 <a href="https://github.com/sahinibrahimli/menimcv/files/476674/SH.CV.pdf" download="newfilename">Download the pdf</a>
 
