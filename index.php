11a25b24cbb92255cf20680dbb4646928ad61f67
sahinibrahimli <sahinibrahimle@gmail.com>
2016-09-16 14:54:56 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
-<html><head><title><?php echo "cool title";?></title></head>
+<html><head><title><?php echo "SEkil";?></title></head>
 <body
 <?php 
 $image_url='https://cloud.githubusercontent.com/assets/22137362/18583178/fc708256-7c19-11e6-90b6-e3816275266a.jpg';
